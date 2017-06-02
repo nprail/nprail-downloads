fd2dbffca89024447be08d526cef338838e06fe2
Noah Prail <nprail@filiosoft.com>
2017-06-02 15:44:29 -0400

    Fixed callback url!

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const strategy = new Auth0Strategy({
         domain: 'nprail.auth0.com',
         clientID: process.env.AUTH0_CLIENT_ID,
         clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        callbackURL: '/callback'
+        callbackURL: process.env.AUTH0_CALLBACK_URL
     },
     function (accessToken, refreshToken, extraParams, profile, done) {
         // accessToken is the token to call Auth0 API (not needed in the most cases)
